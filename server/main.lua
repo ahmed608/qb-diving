fc0b7c85edc98f3e1cba3dcb2c28c2cc80bd269f
konarPlus <57233235+ahmed608@users.noreply.github.com>
2022-08-23 17:45:49 +0200

    add diving fill item
    add diving fill item to fill tube

--- server/main.lua
+++ server/main.lua
@@ -18,7 +18,7 @@ end
 local function hasCoral(src)
     local Player = QBCore.Functions.GetPlayer(src)
     availableCoral = {}
-    for _, v in pairs(Config.CoralTypes) do
+    for k, v in pairs(Config.CoralTypes) do
         local item = Player.Functions.GetItemByName(v.item)
         if item then availableCoral[#availableCoral+1] = v end
     end
@@ -98,22 +98,6 @@ RegisterNetEvent('qb-diving:server:TakeCoral', function(area, coral, bool)
     TriggerClientEvent('qb-diving:client:UpdateCoral', -1, area, coral, bool)
 end)
 
-RegisterNetEvent('qb-diving:server:RemoveGear', function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not Player then return end
-    Player.Functions.RemoveItem("diving_gear", 1)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["diving_gear"], "remove")
-end)
-
-RegisterNetEvent('qb-diving:server:GiveBackGear', function(oxygen)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if oxygen > 0 then
-        Player.Functions.AddItem("diving_gear", 1, false, {['oxygen']=oxygen})
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["diving_gear"], "add")
-    end
-end)
 
 -- Callbacks
 
@@ -121,34 +105,22 @@ QBCore.Functions.CreateCallback('qb-diving:server:GetDivingConfig', function(_, 
     cb(Config.CoralLocations, currentDivingArea)
 end)
 
-QBCore.Functions.CreateCallback('qb-diving:server:RemoveGear', function(src, cb)
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not Player then
-        cb(false)
-        return
-    end
-    local divingGear = Player.Functions.GetItemByName("diving_gear")
-    if divingGear.amount > 0 then
-        local oxygen = 200
-        if divingGear.info.oxygen ~= nil then
-            oxygen = divingGear.info.oxygen
-        end
-        Player.Functions.RemoveItem("diving_gear", 1)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["diving_gear"], "remove")
-        cb(true, oxygen)
-        return
-    end
-    cb(false, 0)
-end)
-
 -- Items
 
+
 QBCore.Functions.CreateUseableItem("diving_gear", function(source)
-    TriggerClientEvent("qb-diving:client:UseGear", source, true)
+    local player = QBCore.Functions.GetPlayer(source)
+        if player ~= nil then 
+            TriggerClientEvent("qb-diving:wearsuit", source)
+        end 
 end)
-
--- Commands
-
+QBCore.Functions.CreateUseableItem("diving_fill", function(source)
+    local player = QBCore.Functions.GetPlayer(source)
+        if player ~= nil then 
+            TriggerClientEvent("qb-diving:setgearoxy", source)
+        end 
+end)
+--commands
 QBCore.Commands.Add("divingsuit", Lang:t("info.command_diving"), {}, false, function(source)
-    TriggerClientEvent("qb-diving:client:UseGear", source, false)
+    TriggerClientEvent("qb-diving:removewearsuit", source, false)
 end)
